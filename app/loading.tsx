904c8d2e4a1d0262bd72f8dcf26a9c5b1eaaf6d3
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2025-01-02 11:20:03 +0530

    feat: updated routes layout

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <h3>Loading...</h3>
+    </div>
+  );
+}
