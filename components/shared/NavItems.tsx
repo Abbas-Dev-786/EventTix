3136edb5fdc3d33f2c5cba75e8e1054a9e81cdd9
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2025-01-02 13:50:28 +0530

    feat: updated navbar

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,31 +1,46 @@
-'use client';
+"use client";
 
-import { headerLinks } from '@/constants'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { useAccount } from "wagmi";
 
 const NavItems = () => {
   const pathname = usePathname();
+  const account = useAccount();
 
   return (
     <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {account?.isConnected && (
+        <li
+          key={"/"}
+          className={`${
+            pathname === "/" && "text-primary-500"
+          } flex-center p-medium-16 whitespace-nowrap`}
+        >
+          <Link href={"/"}>Home</Link>
+        </li>
+      )}
       {headerLinks.map((link) => {
         const isActive = pathname === link.route;
-        
+
         return (
-          <li
-            key={link.route}
-            className={`${
-              isActive && 'text-primary-500'
-            } flex-center p-medium-16 whitespace-nowrap`}
-          >
-            <Link href={link.route}>{link.label}</Link>
-          </li>
-        )
+          account?.address &&
+          link?.isProtected && (
+            <li
+              key={link.route}
+              className={`${
+                isActive && "text-primary-500"
+              } flex-center p-medium-16 whitespace-nowrap`}
+            >
+              <Link href={link.route}>{link.label}</Link>
+            </li>
+          )
+        );
       })}
     </ul>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
--- constants/index.ts
+++ constants/index.ts
@@ -1,27 +1,30 @@
 export const headerLinks = [
   {
-    label: 'Home',
-    route: '/',
+    label: "Home",
+    route: "/",
+    isProtected: false,
   },
   {
-    label: 'Create Event',
-    route: '/events/create',
+    label: "Create Event",
+    route: "/events/create",
+    isProtected: true,
   },
   {
-    label: 'My Profile',
-    route: '/profile',
+    label: "My Profile",
+    route: "/profile",
+    isProtected: true,
   },
-]
+];
 
 export const eventDefaultValues = {
-  title: '',
-  description: '',
-  location: '',
-  imageUrl: '',
+  title: "",
+  description: "",
+  location: "",
+  imageUrl: "",
   startDateTime: new Date(),
   endDateTime: new Date(),
-  categoryId: '',
-  price: '',
+  categoryId: "",
+  price: "",
   isFree: false,
-  url: '',
-}
+  url: "",
+};
